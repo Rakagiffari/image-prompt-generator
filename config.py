bd3977680aaa8369a054fe725865266463c87a41
Rakagiffari <rakagiffari945@gmail.com>
2026-08-30 08:00:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+APP_NAME = "AI Image Prompt Generator"
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
